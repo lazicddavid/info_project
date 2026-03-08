63f7285bd4eee01f29d26b4901db32f1d065e005
David Lazic <lazic.ddavid@gmail.com>
2026-03-08 17:54:34 +0100

    Added age

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ let score = 10;
 score = score + 5;
 
 console.log(score);
+
+const firstName = "David";
+const city = "Belgrade";
+
+console.log(`${firstName} lives in ${city}`);
+
+let age = 20;
+
+if (age >= 18) {
+  console.log("You are too young");
+} else {
+  console.log("You can enter");
+}
