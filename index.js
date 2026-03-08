eb1c2d6a405752c96c80923f9a3fb1a71e1caa08
David Lazic <lazic.ddavid@gmail.com>
2026-03-08 17:16:40 +0100

    Added const userName

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+const userName = "David";
+
+console.log("Hello " + userName);
+
+
+let age = 25;
+let result  = "";
+console.log("25+1")
+
+return result;
+
+
+
+
+const name = "David"; 
+
+const language = "JavaScript";
+
+let plusFunction() {
+  console.log("language" + "is learning +" "name");
+  return plusFunction;
+}
