c01b227903d686b4fda783ca8c46ce5df1821a7c
David Lazic <lazic.ddavid@gmail.com>
2026-03-10 21:25:59 +0100

    added player.play(); player.play(); player.rest();

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const player = {
   name: "David",
+  score: 0,
+  energy: 100,
+
+  play() {
+
+    this.score += 10,
+    this.energy -=5,
+  },
+
+  rest() {
+    this.energy +=20,
+
+  },
+
+  getScore() {
+    console.log(this.score);
+  }
 };
+
+
+player.play();
+player.play();
+player.rest();
+
+console.log(player.getScore());
+console.log(player.energy);
